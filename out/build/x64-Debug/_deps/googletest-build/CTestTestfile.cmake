15c12a9e062306574d1208b1ba81a421ed3ee0d0
MAIPA01 <47190521+MAIPA01@users.noreply.github.com>
2025-08-13 21:38:22 +0200

    Delete out directory

--- out/build/x64-Debug/_deps/googletest-build/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/Users/Patryk/Documents/C++/GLSL-Struct-Cpp/third_party/googletest/3ad1
-# Build directory: C:/Users/Patryk/Documents/C++/GLSL-Struct-Cpp/out/build/x64-Debug/_deps/googletest-build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("googlemock")
